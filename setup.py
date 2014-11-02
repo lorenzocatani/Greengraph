f3da282242e4b3538395d6f3a26414e986c6d3cd
lorenzocatani <tedelori@libero.it>
2014-11-02 16:37:39 +0000

    insert requires in setup

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     name = "Greengraph",
     version = "0.1",
     packages = find_packages(exclude=['*test']) 
+	#install_requires = ['geopy']
+	#install_requires = ['requests']
+	#install_requires = ['png']
+	#install_requires = ['itertools']
+	#install_requires = ['StringIO']
+	#install_requires = ['numpy']
 	)
