98fff8ce262e0755406ee8ce22c6ad9f4b2c0089
lorenzocatani <tedelori@libero.it>
2014-11-02 18:02:02 +0000

    correction on setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name = "Greengraph",
     version = "0.1",
-    packages = find_packages(exclude=['*test']) 
-	#install_requires = ['geopy']
-	#install_requires = ['requests']
-	#install_requires = ['png']
-	#install_requires = ['itertools']
-	#install_requires = ['StringIO']
-	#install_requires = ['numpy']
+    packages = find_packages(exclude=['*test']), 
+	install_requires = ['geopy','PyPng']
 	)
